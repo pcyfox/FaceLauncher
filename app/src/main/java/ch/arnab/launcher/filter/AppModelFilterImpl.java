c3ca19d64325a46397d283fda5c9691c78f0f66a
pcyfox <1037050764@qq.com>
2020-08-21 18:26:44 +0800

    add functions 我

--- app/src/main/java/ch/arnab/launcher/filter/AppModelFilterImpl.java
+++ app/src/main/java/ch/arnab/launcher/filter/AppModelFilterImpl.java
@@ -3,15 +3,15 @@ package ch.arnab.launcher.filter;
 import java.util.ArrayList;
 
 import ch.arnab.launcher.AppModel;
+import ch.arnab.launcher.constant.ConstantData;
 
 public class AppModelFilterImpl implements AppModelFilter {
-    private String[] flags = {"com.taike", "com.tk"};
     @Override
     public ArrayList<AppModel> filer(ArrayList<AppModel> apps) {
         ArrayList<AppModel> newAppModels = new ArrayList<>();
         for (AppModel appModel : apps) {
             String packageName = appModel.getAppInfo().packageName;
-            for (String flag : flags) {
+            for (String flag : ConstantData.TK_APP_FLAGS) {
                 if (packageName.contains(flag) && !packageName.contains("launcher")) {
                     newAppModels.add(appModel);
                     break;
--- app/src/main/java/ch/arnab/launcher/ui/CmdActivity.java
+++ app/src/main/java/ch/arnab/launcher/ui/CmdActivity.java
@@ -4,17 +4,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.blankj.utilcode.constant.PermissionConstants;
-import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.GsonUtils;
 import com.blankj.utilcode.util.PermissionUtils;
 import com.taike.lib_network.udp.UDPSocketClient;
-import com.tk.launcher.BuildConfig;
 import com.tk.launcher.R;
 
 import java.util.List;
@@ -25,62 +28,60 @@ import ch.arnab.launcher.SocketMsgHandler;
 import ch.arnab.launcher.entity.Action;
 import ch.arnab.launcher.entity.LauncherMessage;
 
-public class HomeScreenActivity extends FragmentActivity {
+public class CmdActivity extends FragmentActivity {
     private static final String TAG = "HomeScreenActivity";
-    private AppsGridFragment fragment;
+    private EditText editTextCmd;
+    private LauncherMessage launcherMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homescreen);
-        fragment = (AppsGridFragment) getSupportFragmentManager().findFragmentById(R.id.apps_grid);
+        setContentView(R.layout.activty_cmd);
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         PermissionUtils.permission(PermissionConstants.STORAGE);
-        Log.d(TAG, "onPostCreate() called with: isAppRoot = [" + AppUtils.isAppRoot() + "]");
-        SocketMsgHandler.getInstance().init(this, new SocketMsgHandler.AppModelGetter() {
-            @Override
-            public List<AppModel> get() {
-                return fragment.getAllApps();
-            }
-        });
-        if (BuildConfig.APP_TYPE == 1) {
-            getWindow().getDecorView().postDelayed(new Runnable() {
+        editTextCmd = findViewById(R.id.et_dmd);
+        editTextCmd.setText("{\"action\":\"CLOSE_APP\",\"data\":\"com.taike.edu.stu\"}");
+
+        LinearLayout linearLayout = findViewById(R.id.ll_cmd);
+
+
+        for (final Action action : Action.values()) {
+            RadioButton button = new RadioButton(this);
+            button.setTextColor(ContextCompat.getColor(this, R.color.common_colorAccent));
+            button.setText(action.name());
+            linearLayout.addView(button);
+            button.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void run() {
-                    ActivityUtils.startActivity(CmdActivity.class);
+                public void onClick(View v) {
+                    selectAction(action);
                 }
-            }, 1);
+            });
         }
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_screen, menu);
-        return true;
+
+    private void selectAction(Action action) {
+        launcherMessage = new LauncherMessage(action, ((EditText) findViewById(R.id.et_data)).getText().toString());
+        editTextCmd.setText(GsonUtils.toJson(launcherMessage));
     }
+
 
     @Override
     public void onBackPressed() {
 
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //桌面被干死了，重置登录状态
-        App.Companion.setRegister(false);
-        SocketMsgHandler.getInstance().stop();
-    }
 
-    public void onTest(View view) {
+    public void sendCmd(View view) {
         //{"action":"CLOSE_APP","data":"com.taike.edu.stu"}
         //UDPSocketClient.getInstance().sendBroadcast(GsonUtils.toJson(new LauncherMessage(Action.LAUNCH_APP_SINGLE_TOP, "com.taike.edu.stu")));
-        UDPSocketClient.getInstance().sendBroadcast(GsonUtils.toJson(new LauncherMessage(Action.INSTALL_APP, "http://192.168.28.11:8080/edu_stu.apk")));
+        UDPSocketClient.getInstance().sendBroadcast(editTextCmd.getText().toString());
         //RootUtils.cleatApp("com.taike.edu.stu");
         //RootUtils.uninstallAPK("com.taike.edu.stu");
         // RootUtils.installAPK(this, "/sdcard/debug_TK-Stu_V1.0.0_1_2020-08-20_19-47-52.apk");
